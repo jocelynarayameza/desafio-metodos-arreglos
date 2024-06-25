033790aba8eef64451206e1397cb0890b7d62748
JocelynAraya <arayameza.jocelyn@gmail.com>
2024-06-25 18:25:27 -0400

    checkbox

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const tareas = [
     {
         id: 3,
         descripcion: "Pagar cuentas",
-        realizada: false,
+        realizada: true
     }
 ]
 
@@ -20,30 +20,70 @@ const tareas = [
 const tareaInput = document.querySelector("#input")
 const boton = document.querySelector("#button")
 const tbody = document.querySelector("#tbody")
+const tareasRealizadas = document.querySelector("#realizadasTotal")
 
+const realizadasTotal = () => {
+    let realizadas = 0;
+    tareas.forEach((tarea) => {
+        if (tarea.realizada == true) {
+            realizadas++;
+        }
+    });
+    tareasRealizadas.innerHTML = realizadas;
+}
+
+const actualizarChecked = (id) => {
+    const tareaRealizada = tareas.findIndex((tarea) => tarea.id == id);
+    tareas[tareaRealizada].realizada = !tareas[tareaRealizada].realizada;
+renderizarTabla();
+}
+
+const actualizarTotal = () => {
+    let total = document.querySelector("#valorTotal");
+    total.innerHTML = tareas.length
+}
+
+const renderizarTabla = () => {
 tbody.innerHTML = ''
-
 tareas.forEach((tarea) => {
-   tbody.innerHTML += `<tr>
-    <td> ${tarea.id}</td>
-    <td> ${tarea.descripcion}</td>
-    </tr>`
+    let row = `<tr>
+    <td>${tarea.id}</td>
+    <td>${tarea.descripcion}</td>
+    <td><input type='checkbox' onclick='actualizarChecked(${tarea.id})'></td>`;
+
+    if (tarea.realizada == false) {
+        row += `<td><i class="fa-solid fa-x"></i></td>`;
+
+    } else {
+        row += `<td><i class="fa-solid fa-check"></i></td>`;
+    }
+    row += `</tr>`;
+    tbody.innerHTML += row;
 
 });
+actualizarTotal();
+realizadasTotal();
+}
 
-boton.addEventListener("click", function() {
+renderizarTabla();
+
+
+
+boton.addEventListener("click", function () {
     const nuevaDescripcion = tareaInput.value;
     const nuevaTarea = {
         id: tareas.length + 1,
         descripcion: nuevaDescripcion,
         realizada: false,
     };
-    tareas.push(nuevaTarea);
-    tbody.innerHTML += `<tr>
-        <td>${nuevaTarea.id}</td>
-        <td>${nuevaTarea.descripcion}</td>
-    </tr>`;
-    tareaInput.value = '';
+    if (nuevaDescripcion === "") {
+        alert("no se puede agregar una tarea vacía")
+    } else {
+        tareas.push(nuevaTarea);
+        renderizarTabla();
+        tareaInput.value = '';
+    }
+
 });
 
-//AGREGAR LOGICA: SI NO SE ESCRIBE NADA NO AGREGAR NADA
+
